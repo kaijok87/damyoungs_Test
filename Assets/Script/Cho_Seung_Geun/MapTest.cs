dc5fe0c21e1808ab547d29730cc4c391385a238b
ChoSeungGeun <bummykimmy@gmail.com>
2023-06-21 15:21:37 +0900

    맵 테스트 수정(2)

--- Assets/Script/Cho_Seung_Geun/MapTest.cs
+++ Assets/Script/Cho_Seung_Geun/MapTest.cs
@@ -7,6 +7,9 @@ using UnityEngine.InputSystem;
 
 public class MapTest : TestBase
 {
+    
+
+
     public GameObject centerTile;           // 중앙에 사용할 타일
     public GameObject sideTile;             // 외곽에 배치될 타일
     public GameObject vertexTile;           // 꼭지점 타일
@@ -22,19 +25,21 @@ public class MapTest : TestBase
     bool isExist = false;                   // 타일 존재 여부
 
     Vector3 mainTileSize = Vector3.zero;    // 중앙 타일 사이즈
-    Vector3 sideTileSize = Vector3.zero;    // 외곽 타일 사이즈
+    Vector3 sideTileSize = Vector3.zero;    // 사이드 타일 사이즈
     Vector3 vertexTileSize = Vector3.zero;  // 꼭지점 타일 사이즈
-    Vector3 wallSize = Vector3.zero;        // 벽 사이즈
+
     Vector3 startPos = new Vector3();       // 추후에 캐릭터 놓을 위치. 지금은 임시적으로 (0, 0, 0) 으로 설정
 
     GameObject[] mapTiles;                // 타일 오브젝트 객체를 담을 배열(혹시나 싶어 이중배열 외에 하나로 하는 배열을 남겨둠)
     //GameObject[,] mapTiles;                 // 타일 오브젝트 객체를 담을 이중 배열
+    GameObject[] walls;
+
 
     private void Start()
     {
         // 중앙 타일 사이즈 반환
         mainTileSize = centerTile.GetComponentInChildren<BoxCollider>().size * centerTile.transform.GetChild(0).localScale.x;
-        // 외곽 타일 사이즈 반환
+        // 사이드 타일 사이즈 반환
         sideTileSize = sideTile.GetComponentInChildren<BoxCollider>().size * sideTile.transform.GetChild(0).localScale.x;      
         // 꼭지점 타일 사이즈 반환
         vertexTileSize = vertexTile.GetComponentInChildren<BoxCollider>().size * vertexTile.transform.GetChild(0).localScale.x;
@@ -51,21 +56,24 @@ public class MapTest : TestBase
             sizeY = Random.Range(10, 21);       // 타일 세로 갯수 랜덤 생성
             tileCount = sizeX * sizeY;          // 총 타일 갯수
             mapTiles = new GameObject[tileCount];   // 배열 동적 생성
-        
+            
+            //walls = new GameObject[]
         
             for (int i = 0; i < tileCount; i++)
             {
                 int width = i % sizeX;
                 int length = i / sizeX;
 
-                // 사이드 타일 생성
+                // 타일 생성
                 if ((width == 0 && length == 0) || (width == 0 && length == sizeY - 1) || (width == sizeX - 1 && length == 0) || (width == sizeX - 1 && length == sizeY - 1))
                 {
-                    mapTiles[i] = Instantiate(vertexTile, gameObject.transform);
+                    mapTiles[i] = Instantiate(vertexTile, gameObject.transform);            // 꼭지점 타일 생성
+                    mapTiles[i].GetComponent<Tile>().Type = (int)TileType.vertexTile;       // 타일 스크립트에 타입 저장
                 }
                 else if (width == 0 || width == sizeX - 1 || length == 0 || length == sizeY - 1)              // 사이드 타일 회전
                 {
-                    mapTiles[i] = Instantiate(sideTile, gameObject.transform);
+                    mapTiles[i] = Instantiate(sideTile, gameObject.transform);              // 사이드 타일 생성
+                    mapTiles[i].GetComponent<Tile>().Type = (int)TileType.sideTile;         // 타일 스크립트에 타입 저장
                     if (width == 0)                                                             // 왼쪽 세로줄
                     {
                         mapTiles[i].transform.Rotate(new Vector3(0, 90.0f, 0));
@@ -85,8 +93,9 @@ public class MapTest : TestBase
                 }
                 else
                 {
-                    mapTiles[i] = Instantiate(centerTile, gameObject.transform);                         // 중앙 타일 생성
-                    mapTiles[i].transform.Rotate(new Vector3(0, 90.0f * Random.Range(0, 4), 0));         // 중앙 타일 랜덤 회전(그냥 미관상)
+                    mapTiles[i] = Instantiate(centerTile, gameObject.transform);                        // 중앙 타일 생성
+                    mapTiles[i].transform.Rotate(new Vector3(0, 90.0f * Random.Range(0, 4), 0));        // 중앙 타일 랜덤 회전(그냥 미관상)
+                    mapTiles[i].GetComponent<Tile>().Type = (int)TileType.centerTile;                   // 타일 스크립트에 타입 저장
                 }
 
                 // 타일 위치 이동. startPos는 임시로 넣어놓은 값(0, 0, 0)
@@ -96,6 +105,9 @@ public class MapTest : TestBase
         
             isExist = true;
         }
+
+
+
 
         //if (!isExist)                   // 타일이 존재하지 않을 경우에만 생성
         //{
--- Assets/Script/Cho_Seung_Geun/Tile.cs
+++ Assets/Script/Cho_Seung_Geun/Tile.cs
@@ -2,8 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum TileType
+{
+    centerTile = 0,
+    sideTile,
+    vertexTile
+}
+
 public class Tile : MonoBehaviour
 {
     
 
+    TileType type = 0;
+    //public int Type
+    //{
+    //    get => type;
+    //    set
+    //    {
+    //        type = value;
+    //    }
+
+    //}
+    public int Type
+    {
+        get => (int)type;
+        set
+        {
+            type = (TileType)value;
+        }
+
+    }
 }
