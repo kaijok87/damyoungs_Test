c9917f32bd3234aff21ba05a9f0bc53bee7b48a7
ChoSeungGeun <bummykimmy@gmail.com>
2023-06-23 15:20:06 +0900

    맵 테스트 수정(3)

--- Assets/Script/Cho_Seung_Geun/MapTest.cs
+++ Assets/Script/Cho_Seung_Geun/MapTest.cs
@@ -7,13 +7,9 @@ using UnityEngine.InputSystem;
 
 public class MapTest : TestBase
 {
-    
-
-
     public GameObject centerTile;           // 중앙에 사용할 타일
     public GameObject sideTile;             // 외곽에 배치될 타일
     public GameObject vertexTile;           // 꼭지점 타일
-
     public GameObject wall;                 // 벽
 
 
@@ -21,6 +17,7 @@ public class MapTest : TestBase
     int sizeY = 0;                          // 타일 세로 갯수
 
     public int tileCount = 0;               // 타일의 수
+    public int wallCount = 0;
 
     bool isExist = false;                   // 타일 존재 여부
 
@@ -30,9 +27,9 @@ public class MapTest : TestBase
 
     Vector3 startPos = new Vector3();       // 추후에 캐릭터 놓을 위치. 지금은 임시적으로 (0, 0, 0) 으로 설정
 
-    GameObject[] mapTiles;                // 타일 오브젝트 객체를 담을 배열(혹시나 싶어 이중배열 외에 하나로 하는 배열을 남겨둠)
-    //GameObject[,] mapTiles;                 // 타일 오브젝트 객체를 담을 이중 배열
-    GameObject[] walls;
+    GameObject[] mapTiles;                // 타일 오브젝트 객체를 담을 배열
+    List<GameObject> walls;
+    //GameObject[] walls;
 
 
     private void Start()
@@ -56,24 +53,56 @@ public class MapTest : TestBase
             sizeY = Random.Range(10, 21);       // 타일 세로 갯수 랜덤 생성
             tileCount = sizeX * sizeY;          // 총 타일 갯수
             mapTiles = new GameObject[tileCount];   // 배열 동적 생성
-            
-            //walls = new GameObject[]
-        
+
+            wallCount = 2 * sizeX + 2 * sizeY - 4;
+            //walls = new GameObject[wallCount];      // 벽 타일 갯수 생성
+            walls = new List<GameObject>(wallCount);
+
+            GameObject wallObject;
+
             for (int i = 0; i < tileCount; i++)
             {
-                int width = i % sizeX;
-                int length = i / sizeX;
+                int width = i % sizeX;              // 가로 인덱스 번호
+                int length = i / sizeX;             // 세로 인덱스 번호
 
                 // 타일 생성
                 if ((width == 0 && length == 0) || (width == 0 && length == sizeY - 1) || (width == sizeX - 1 && length == 0) || (width == sizeX - 1 && length == sizeY - 1))
                 {
+
+
                     mapTiles[i] = Instantiate(vertexTile, gameObject.transform);            // 꼭지점 타일 생성
                     mapTiles[i].GetComponent<Tile>().Type = (int)TileType.vertexTile;       // 타일 스크립트에 타입 저장
+                    wallObject = Instantiate(wall, mapTiles[i].transform);
+                    wallObject.transform.Translate(new Vector3(1.0f, 0.0f, -1.7f));
+                    //walls.Add(wallObject);
+                    wallObject = Instantiate(wall, mapTiles[i].transform);
+                    wallObject.transform.Translate(new Vector3(1.7f, 0.0f, 1.0f));
+                    //walls.Add(wallObject);
+                    if (width == 0 && length == 0)
+                    {
+                        mapTiles[i].transform.Rotate(new Vector3(0, 180.0f, 0));
+                    }
+                    else if (width == 0 && length == sizeY - 1)
+                    {
+                        mapTiles[i].transform.Rotate(new Vector3(0, 270.0f, 0));
+                    }
+                    else if (width == sizeX - 1 && length == 0)
+                    {
+                        mapTiles[i].transform.Rotate(new Vector3(0, 90.0f, 0));
+                    }
+                    else if (width == sizeX - 1 && length == sizeY - 1)
+                    {
+                        mapTiles[i].transform.Rotate(new Vector3(0, 360.0f, 0));
+                    }
                 }
                 else if (width == 0 || width == sizeX - 1 || length == 0 || length == sizeY - 1)              // 사이드 타일 회전
                 {
                     mapTiles[i] = Instantiate(sideTile, gameObject.transform);              // 사이드 타일 생성
                     mapTiles[i].GetComponent<Tile>().Type = (int)TileType.sideTile;         // 타일 스크립트에 타입 저장
+                    wallObject = Instantiate(wall, mapTiles[i].transform);
+                    wallObject.transform.Translate(new Vector3(1, 0.0f, -1.7f));
+                    //walls.Add(wallObject);
+
                     if (width == 0)                                                             // 왼쪽 세로줄
                     {
                         mapTiles[i].transform.Rotate(new Vector3(0, 90.0f, 0));
@@ -103,62 +132,18 @@ public class MapTest : TestBase
                                                             0, startPos.z + mainTileSize.z * sizeY - mainTileSize.z * length);
             }
         
+            //for (int i = 0; i < wallCount; i++)
+            //{
+            //    wallObject = Instantiate(wall);
+            //    wallObject.transform.position = new Vector3(mapTiles[0].transform.position.x + 1.0f,
+            //                                                mapTiles[0].transform.position.y,
+            //                                                mapTiles[0].transform.position.z + 1.7f);
+            //    //walls.Add(wallObject);
+            //}
+
             isExist = true;
         }
 
-
-
-
-        //if (!isExist)                   // 타일이 존재하지 않을 경우에만 생성
-        //{
-        //    sizeX = Random.Range(10, 21);       // 타일 가로 갯수 랜덤 생성
-        //    sizeY = Random.Range(10, 21);       // 타일 세로 갯수 랜덤 생성
-        //    tileCount = sizeX * sizeY;          // 총 타일 갯수
-        //    mapTiles = new GameObject[sizeX, sizeY];   // 배열 동적 생성
-
-
-        //    for (int j = 0; j < sizeY; j++)                             // 타일 생성 i = 가로 방향으로 증가, j = 세로 방향으로 증가
-        //    {
-        //        for (int i = 0; i < sizeX; i++)
-        //        {
-        //            // 사이드 타일 생성
-        //            if ((i == 0 && j == 0) || (i == 0 && j == sizeY - 1) || (i == sizeX - 1 && j == 0) || (i == sizeX - 1 && j == sizeY - 1))
-        //            {
-        //                mapTiles[i, j] = Instantiate(vertexTile);
-        //            }
-        //            else if (i == 0 || i == sizeX - 1 || j == 0 || j == sizeY - 1)              // 사이드 타일 회전
-        //            {
-        //                mapTiles[i, j] = Instantiate(sideTile);
-        //                if (i == 0)                                                             // 왼쪽 세로줄
-        //                {
-        //                    mapTiles[i, j].transform.Rotate(new Vector3(0, 90.0f, 0));
-        //                }
-        //                else if (i == sizeX - 1)                                                // 오른쪽 세로줄
-        //                {
-        //                    mapTiles[i, j].transform.Rotate(new Vector3(0, 270.0f, 0));
-        //                }
-        //                else if (j == 0)                                                        // 맨 윗줄
-        //                {
-        //                    mapTiles[i, j].transform.Rotate(new Vector3(0, 180.0f, 0));
-        //                }
-        //                //else if (j == sizeY - 1)                                              // 맨 아랫줄
-        //                //{
-        //                //    mapTiles[i, j].transform.Rotate(new Vector3(0, 360.0f, 0));
-        //                //}
-        //            }
-        //            else
-        //            {
-        //                mapTiles[i, j] = Instantiate(centerTile);                                               // 중앙 타일 생성
-        //                mapTiles[i, j].transform.Rotate(new Vector3(0, 90.0f * Random.Range(0, 4), 0));         // 중앙 타일 랜덤 회전(그냥 미관상)
-        //            }
-        //            // 타일 위치 이동. startPos는 임시로 넣어놓은 값(0, 0, 0)
-        //            mapTiles[i, j].transform.position = new Vector3(startPos.x - mainTileSize.x * sizeX / 2 + mainTileSize.x * i,
-        //                                                            0, startPos.z + mainTileSize.z * sizeY - mainTileSize.z * j);
-        //        }
-        //    }
-
-        //    isExist = true;             // 타일 생성 여부(타일이 생성되어 있다)
-        //}
     }
 
     /// <summary>
@@ -171,13 +156,7 @@ public class MapTest : TestBase
             Destroy(mapTiles[i]);
         }
 
-        //for (int i = 0; i < sizeX; i++)
-        //{
-        //    for(int j = 0;j < sizeY; j++)
-        //    {
-        //        Destroy(mapTiles[i, j]);
-        //    }
-        //}
+
         isExist = false;
     }
 }
